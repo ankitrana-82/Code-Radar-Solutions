04df332c74c6ee56def396ee37669bd988684d4b
ankitrana-82 <ankit0175.becse24@chitkara.edu.in>
2025-01-25 07:55:37 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,20 +6,20 @@ int main(){
     switch(c){
         case '+':
             result=a+b;
-            printf("%.2lf",result);
+            printf("%.2g",result);
             break;
         case '-':
             result=a-b;
-            printf("%.2lf",result);
+            printf("%.2g",result);
             break;
         case '*':
             result=a*b;
-            printf("%.2lf",result);
+            printf("%.2g",result);
             break;
         case '/':
             if(b!=0){
                 result=a/b;
-                printf("%2.lf",result);
+                printf("%2.g",result);
             } else{
                 printf("error");
             }
